05ed3377baa6c5e1fa6e63379844391f00ea4712
NazarElf <nnn43@ukr.net>
2018-01-08 19:32:36 +0200

    Visual changes
    Added output and table

--- CM_Lab4/Program.cs
+++ CM_Lab4/Program.cs
@@ -39,24 +39,36 @@ namespace CM_Lab4
             }
 
             double[] F5A = new double[16];
-            double x4 = 0.0;
+            double[] x4 = new double[16];
             for (int i = 0; i < 16; i++)
             {
-                F5A[i] = F(f4, F4A, i, out x4);
+                F5A[i] = F(f4, F4A, i, out x4[i]);
             }
 
             Console.WriteLine("Max res = " + F5A[15]);
 
-            Console.WriteLine("x4 = " + x4);
-            double X2 = x2[x2.Length - 1 - (int)x4];
-            int _2 = x2.Length - 1 - (int)x4;
+            Console.WriteLine("x4 = " + x4[15]);
+            double X2 = x2[x2.Length - 1 - (int)x4[15]];
+            int _2 = x2.Length - 1 - (int)x4[15];
             Console.WriteLine("x2 = " + X2);
             double X1 = x1[_2 - (int)X2];
             int _1 = _2 - (int)X2;
             Console.WriteLine("x1 = " + X1);
             double X5 = x5[_1 - (int)X1];
             Console.WriteLine("x5 = " + X5);
-            Console.WriteLine("x3 = " + (15 - X5 - x4 - X2 - X1));
+            Console.WriteLine("x3 = " + (15 - X5 - x4[15] - X2 - X1));
+            Console.WriteLine("A\tF1A\tx3\tF2A\tx5\tF3A\tx1\tF4A\tx2\tF5A\tx4");
+            for (int i = 0; i < 16; i++)
+            {
+                string wr = i + "\t";
+                if (i < 10)
+                    wr += F1A[i] + "\t" + i + "\t" + F2A[i] + "\t" + x5[i] + "\t";
+                else
+                    wr += F1A[9] + "\t" + 9 + "\t" + F2A[9] + "\t" + x5[9] + "\t";
+                wr += F3A[i] + "\t" + x1[i] + "\t" + F4A[i] + "\t" + x2[i] + "\t" + F5A[i] +"\t" + x4[i];
+                Console.WriteLine(wr);
+            }
+            Console.Read();
         }
         
         static double F(Func<int, double> f, double[] F, int n, out double x)
